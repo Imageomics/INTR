c9084607bd61ac5fddf7ee048717b1742524c760
dipanjyoti <dipanjyotipaul@gmail.com>
2023-11-05 00:49:27 -0400

    initial commit ...

--- engine.py
+++ engine.py
@@ -1,6 +1,6 @@
 # ------------------------------------------------------------------------
 # INTR
-# Copyright (c) 2023 PAUL. All Rights Reserved.
+# Copyright (c) 2023 Imageomics Paul. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 [see LICENSE for details]
 # ------------------------------------------------------------------------
 # Copied from DETR (https://github.com/facebookresearch/detr)
